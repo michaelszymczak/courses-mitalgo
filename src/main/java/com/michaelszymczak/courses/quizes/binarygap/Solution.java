ef53800ece4e3d7a5e282d5db1686caa5ef8f2d6
Michael Szymczak <contact@michaelszymczak.com>
2016-10-29 19:00:17 +0100

    Binary gap with O(log(n)) time complexity (works)

---
+++ src/main/java/com/michaelszymczak/courses/quizes/binarygap/Solution.java
@@ -0,0 +1,39 @@
+package com.michaelszymczak.courses.quizes.binarygap;
+
+class Solution {
+    public int solution(int N) {
+        return new Data(N)
+                .withAllTrailingZerosStripped()
+                .withAllCalculationsDone()
+                .result;
+    }
+
+    private static class Data {
+
+        final int input, count, result;
+
+        Data(int input) { this.input = input; this.count = 0; this.result = 0; }
+        Data(int input, int count, int result) { this.input = input; this.count = count; this.result = result; }
+
+
+        Data withAllTrailingZerosStripped() {
+            return !isZeroNext() ? asIs() : withNextTrailingZeroStripped().withAllTrailingZerosStripped();
+        }
+
+        Data withAllCalculationsDone() {
+            return nothingLeft() ? asIs() : withNextStepCalculated().withAllCalculationsDone();
+        }
+
+        Data withNextStepCalculated() {
+            return isZeroNext() ? nextIncrementingCount() : sumUpAndResetCount();
+        }
+
+        Data asIs() { return this; }
+        boolean isZeroNext() { return input != 0 && input % 2 == 0; }
+        boolean nothingLeft() { return input == 0; }
+        Data nextIncrementingCount() { return new Data(input / 2, count + 1, result); }
+        Data sumUpAndResetCount() { return new Data(input / 2, 0, count > result ? count : result); }
+        Data withNextTrailingZeroStripped() { return new Data(input / 2, count, result); }
+
+    }
+}
---
+++ src/test/groovy/com/michaelszymczak/courses/quizes/binarygap/BinaryGapShould.groovy
@@ -0,0 +1,32 @@
+package com.michaelszymczak.courses.quizes.binarygap
+
+import spock.lang.Specification
+import spock.lang.Timeout
+
+import java.util.concurrent.TimeUnit
+
+class BinaryGapShould extends Specification {
+
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    def "should count the longest sequence of zeros surrounded by ones"() {
+        expect:
+        gap(input) == expectdOutput
+
+        where:
+        input                    | expectdOutput
+        "0"                      | 0
+        "1"                      | 0
+        "10"                     | 0
+        "01"                     | 0
+        "101"                    | 1
+        "1001"                   | 2
+        "110101010111100"        | 1
+        "1101010001011110000000" | 3
+        "1" + "0" * 29 + "1"     | 29
+
+    }
+
+    private static int gap(String binaryRepresentation) {
+        new Solution().solution(Integer.parseInt(binaryRepresentation, 2))
+    }
+}
