08843dcf42cf83980d31c40f97b29ebf6fa767e3
Michael Szymczak <contact@michaelszymczak.com>
2016-10-30 20:35:26 +0000

    [Solved] Tape equilibrium in O(n) time and O(1) memory.

--- src/main/java/com/michaelszymczak/courses/quizes/TapeEquilibrium.java
+++ src/main/java/com/michaelszymczak/courses/quizes/TapeEquilibrium.java
@@ -1,4 +1,24 @@
 package com.michaelszymczak.courses.quizes;
 
+import java.util.Arrays;
+
 public class TapeEquilibrium {
+    public int solution(int[] A) {
+
+        int result = Integer.MAX_VALUE;
+        int r = sumOfAllIn(A);
+        int l = 0;
+
+        for (int pos = 0; pos < A.length - 1; pos++) {
+            l += A[pos];
+            r -= A[pos];
+            result = result > Math.abs(l - r) ? Math.abs(l - r) : result;
+        }
+
+        return result;
+    }
+
+    private static int sumOfAllIn(int[] A) {
+        return Arrays.stream(A).sum();
+    }
 }
--- src/test/groovy/com/michaelszymczak/courses/quizes/TapeEquilibriumTest.groovy
+++ src/test/groovy/com/michaelszymczak/courses/quizes/TapeEquilibriumTest.groovy
@@ -3,4 +3,16 @@ package com.michaelszymczak.courses.quizes
 import spock.lang.Specification
 
 class TapeEquilibriumTest extends Specification {
+
+    def "should calculate equilibrium"() {
+        expect:
+        new TapeEquilibrium().solution(A as int[]) == result
+
+        where:
+        result | A
+        1      | [3, 1, 2, 4, 3]
+        0      | [3, 1, 2, 3, 1, 2]
+        1      | [3, 4, 3, 2, 1]
+        97     | [3, 100]
+    }
 }
