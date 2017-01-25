f29d3d16d30ac01ba6f9249bcc12cf4cffd4bfe9
Michael Szymczak <contact@michaelszymczak.com>
2017-01-25 21:43:48 +0000

    MaxGain: case with monotonically increasing pricesover time

--- src/main/java/com/michaelszymczak/courses/mitalgo/chapter04/MaxProfit.java
+++ src/main/java/com/michaelszymczak/courses/mitalgo/chapter04/MaxProfit.java
@@ -1,17 +1,32 @@
 package com.michaelszymczak.courses.mitalgo.chapter04;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class MaxProfit
 {
+    private static final int NO_PROFIT = 0;
+
+    private final int[] sequence;
+
     public static MaxProfit of(int... sequence)
     {
-        return new MaxProfit();
+        return new MaxProfit(sequence);
     }
 
-    private MaxProfit() {
-
+    private MaxProfit(int... sequence) {
+        this.sequence = Arrays.copyOf(sequence, sequence.length);
     }
 
     public Integer profit() {
-        return 0;
+        return isEnoughTimeToBuyAndSell() ? calculatedProfit() : NO_PROFIT;
+    }
+
+    private int calculatedProfit() {
+        return IntStream.of(sequence).max().getAsInt() - sequence[0];
+    }
+
+    private boolean isEnoughTimeToBuyAndSell() {
+        return sequence.length > 1;
     }
 }
--- src/test/groovy/com/michaelszymczak/courses/mitalgo/chapter04/MaxProfitTest.groovy
+++ src/test/groovy/com/michaelszymczak/courses/mitalgo/chapter04/MaxProfitTest.groovy
@@ -9,4 +9,18 @@ class MaxProfitTest extends Specification {
         MaxProfit.of(5).profit() == 0
         MaxProfit.of().profit() == 0
     }
+
+    def "should sell at maximum price after buying"() {
+        given:
+        int[] prices = inputPrices
+
+        expect:
+        MaxProfit.of(prices).profit() == expectedMaxProfit
+
+        where:
+        inputPrices        | expectedMaxProfit
+        [2, 3]             | 1
+        [2, 2, 2]          | 0
+        [1, 5, 3, 7, 4, 6] | 6
+    }
 }
